a3dc14e87e9186bb2f2375191b524e7be35976bb
Krzysztof Rosinski <rosek86@gmail.com>
2019-01-01 15:34:41 +0100

    Add c++ guards

--- src/sds011_fifo.h
+++ src/sds011_fifo.h
@@ -5,6 +5,10 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
   uint8_t *mem;
   size_t size;
@@ -17,5 +21,9 @@ typedef struct {
 bool sds011_fifo_init(sds011_fifo_t *fifo, size_t elsize, void *mem, size_t size);
 bool sds011_fifo_push(sds011_fifo_t *fifo, void *el);
 bool sds011_fifo_pop(sds011_fifo_t *fifo, void *el);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // SDS011_FIFO_H__
--- src/sds011_validator.h
+++ src/sds011_validator.h
@@ -4,6 +4,14 @@
 #include "sds011_common.h"
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool sds011_validator_validate(sds011_msg_t const *req, sds011_msg_t const *res);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // SDS011_VALIDATOR_H__
